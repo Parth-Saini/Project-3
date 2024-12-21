505d63c11a4748aa91ea938805878bb838f7baff
Parth-Saini <sainiparth565@gmail.com>
2024-12-21 22:16:48 +0530

    updated

---
+++ toggle.js
@@ -0,0 +1,25 @@
+const toggleCheckbox = document.getElementById('myToggle');
+
+const contentOne = document.getElementById('content1');
+
+const contentTwo = document.getElementById('content2');
+
+
+
+toggleCheckbox.addEventListener('change', () => {
+
+  if (toggleCheckbox.checked) {
+
+    contentOne.style.display = 'none';
+
+    contentTwo.style.display = 'block';
+
+  } else {
+
+    contentOne.style.display = 'block';
+
+    contentTwo.style.display = 'none';
+
+  }
+
+});
